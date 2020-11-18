8c019db78dee831415209d7a5ef4867586e4b055
AymericALLAIN <allainaymeric2@neuf.fr>
2020-11-18 15:08:38 +0100

    ajout de la classe Verrou

--- Verrou.java
+++ Verrou.java
@@ -1,4 +1,42 @@
 public class Verrou
 {
+	private boolean verrouille; 
+
+	public Verrou()
+	{
+		this.verrouille = false;
+	}
 	
+	public void verrouiller() throws InterruptedException
+	{
+		while(this.estVerrouille())
+		{
+			//On attend le déverrouillage
+			//On met en pause le processus pendant x ms ( ici 10ms )
+			Thread.sleep(10);
+		}
+		
+		this.verrouille = true;
+	}
+	
+	public void deverouiller()
+	{
+		this.verrouille = false;
+	}
+	
+	public boolean estVerrouille()
+	{
+		return this.verrouille;
+	}
+	
+	public boolean essaiVerrouillage() throws InterruptedException
+	{
+		if(!this.estVerrouille())
+		{
+			this.verrouiller();
+			return true;
+		}
+		
+		return false;
+	}
 }
